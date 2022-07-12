64c649258a41d3dc87d695cf3d6ec5e7512960d5
Ryannnkl <ryannnkl@gmail.com>
2022-07-12 18:44:51 -0300

    add github man call to action button on main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Link from 'next/link'
+import { FiGithub } from 'react-icons/fi'
+
 import styles from '../styles/Home.module.css'
 
-import NavBar from "../components/NavBar";
+import NavBar from '../components/NavBar';
 import Layout from '../components/Layout';
 
 const Home: NextPage = () => {
@@ -33,10 +35,10 @@ const Home: NextPage = () => {
             Criando experiências na Web usando tecnologias modernas de Frontend e Backend
           </h1>
           <p className="my-4">Desenvolvedor Full Stack na Primata e contribuidor de projetos open-source como o Gnome</p>
-          <p className="my-4">Para entrar em contato use o email: <Link href="mailto:ryannnkl@gmail.com" passHref><a className="text-emerald-600">ryannnkl@gmail.com</a></Link> ou entre em contato pelo instagram: <Link href="https://instagram.com/ryann_ferreira_"><a target="_blank" className="text-emerald-600">ryann_ferreira_</a></Link> </p>
-          <Link href="/">
-          <p 
-    className="py-4 px-3 border w-fit rounded-xl cursor-pointer hover:border-emerald-600 hover:text-emerald-600 hover:shadow duration-150 hover:shadow-emerald-600">Whatsapp</p>
+          <p className="my-4">Para entrar em contato use o email: <Link href="mailto:ryannnkl@gmail.com" passHref><a className="text-emerald-600">ryannnkl@gmail.com</a></Link> ou entre em contato pelo Instagram: <Link href="https://instagram.com/ryann_ferreira_"><a target="_blank" className="text-emerald-600">ryann_ferreira_</a></Link> </p>
+    <Link href="https://github.com/Ryannnkl" passHref>
+          <a target="_blank" 
+    className="flex py-4 px-3 border w-fit rounded-xl cursor-pointer hover:shadow duration-150 hover:shadow-neutral-500"><FiGithub size={20} color="#e5e5e5" className="mr-2"/>Github</a>
           </Link>
         </div>
       </section>
